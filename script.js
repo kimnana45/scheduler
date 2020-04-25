281f2063f16a5cfabc42c9b43434fd5e89920720
Kim Le <kimle.lek@gmail.com>
2020-04-25 14:59:29 -0400

    daily planner with moment.js

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
 // Use moment to get today's date
 // format should be [DAY OF WEEK], [Full Month] [DAYOFMONTH ex 3rd]
 var currentDay = $("#currentDay").text(moment().format("dddd MMMM Do"));
-console.log(typeof currentDay);
 var currentTime = moment().format("HH");
-// var dayOf = localStorage.setItem("currentDay", currentDay);
 var storedDate = localStorage.getItem('currentDay');
-console.log(storedDate);
-console.log(currentDay);
-// Use moment to get current time (hours) and compare to 9AM-5PM hardcoded
-// $(document).ready(function () {
-//         var storedDate = localStorage.getItem('currentDay');
-//     if (storedDate == currentDay){
-//         for (let l = 0; l < localStorage.length; l++)
-//         {
-//             let key = localStorage.key(l);
-//             let value = localStorage.getItem(key);
-//             var textAreaId = "#text" + key;
-//             $(textAreaId).val(value);
-//         }
-//     } else {
-//         localStorage.clear();
-//         var dateInLocalStorage = localStorage.setItem('currentDay', currentDay);
-//     }
-// });
+// console.log(storedDate);
+// console.log(currentDay);
 
 function setTimeColor() {
     for (var i = 9; i <= 17; i++) {
@@ -60,18 +42,16 @@ setTimeColor();
 
 $(".saveBtn").on("click", function (e) {
     e.preventDefault();
-    //console.log("something");
+    // console.log("something");
     var textValue = $(this).siblings(".text").val();
     // console.log(textValue);
     var timeValue = $(this).siblings(".hour").attr("id");
-    //console.log(timeValue)
+    // console.log(timeValue);
     localStorage.setItem(timeValue, textValue);
 
 });
+//compare the date with the data being saved. If the date is current day, then retrieve and display data
 if (storedDate == currentDay) {
-    // for (t = 09; t < 18; t++ ){
-    //     $(this).val(localStorage.getItem(t));
-    // }
     $('#text9').val(localStorage.getItem('09') || "");
     $('#text10').val(localStorage.getItem('10') || "");
     $('#text11').val(localStorage.getItem('11') || "");
@@ -81,7 +61,6 @@ if (storedDate == currentDay) {
     $('#text15').val(localStorage.getItem('15') || "");
     $('#text16').val(localStorage.getItem('16') || "");
     $('#text17').val(localStorage.getItem('17') || "");
-
 } else {
     localStorage.clear();
     var dateInLocalStorage = localStorage.setItem('currentDay', currentDay);
